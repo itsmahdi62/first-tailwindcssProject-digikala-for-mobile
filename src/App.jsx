ae97cb678d4f972d41d9f37f9fe518984a00eda5
Mahdi <amiralmasi021@gmail.com>
2023-05-03 21:01:27 +0330

    Add tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {FaBars} from "react-icons/fa"
+function App() {
+  return (
+    <div className="App bg-gray-400 w-screen h-screen ">
+       <div className="w-full h-44 bg-green-300 flex flex-cole">
+        <div className='flex'><FaBars /></div>
+       </div>
+    </div>
+  );
+}
+
+export default App;
